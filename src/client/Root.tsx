398b1733c7ede7da6c7b3412b4045811661a38ff
PD Jell <jellive7@gmail.com>
2018-05-17 15:36:43 +0900

    import RouteComponentProps.

--- src/client/Root.tsx
+++ src/client/Root.tsx
@@ -2,10 +2,11 @@ import * as React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import App from '../shared/App';
 
+
 const Root = (): JSX.Element => {
   return (
     <BrowserRouter>
-      <App />
+      <App history={history}/>
     </BrowserRouter>
   );
 };
--- src/shared/App.tsx
+++ src/shared/App.tsx
@@ -1,37 +1,42 @@
+import * as H from 'history';
 import { Drawer, MenuItem } from 'material-ui';
 import { MuiThemeProvider } from 'material-ui/styles';
 import * as React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Router, Switch  } from 'react-router-dom';
 import Menu from '../components/Menu';
 import { About, Home, Post } from '../pages/index';
 import Test from '../pages/Test'
 
 
 interface Props {
-
+  history?: any
 }
 
 interface State {
-  SplitMe: () => JSX.Element
-  open: boolean
+  SplitMe?: () => JSX.Element
+  open: boolean,
+  history: H.History
 }
 
 class App extends React.Component<Props, State> {
 
   constructor(props: Props) {
     super(props)
+    this.state = {
+      open: false,
+      history: H.createBrowserHistory()
+    }
   }
 
-  componentWillMount() {
-    this.setState({
-      open: false
-    })
-  }
-
-  handleMenuBtn = (open: boolean): void => {
+  handleMenuBtnClicked = (open: boolean): void => {
     this.setState({
       open: open
     })
+  }
+
+  handleSideBtnClicked = (menu: string): void => {
+    console.log(this.props.history)
+      this.state.history.push(menu)
   }
 
   public render() {
@@ -43,16 +48,17 @@ class App extends React.Component<Props, State> {
 
     return (
       <MuiThemeProvider>
+      <Router history={this.state.history}>
         <div style={{ height: '100%' }}>
-          <Menu leftClick={() => this.handleMenuBtn(!this.state.open)} />
+          <Menu leftClick={() => this.handleMenuBtnClicked(!this.state.open)} />
           <Drawer
             docked={false}
             width={200}
             open={this.state.open}
-            onRequestChange={(open) => this.handleMenuBtn(open)}
+            onRequestChange={(open) => this.handleMenuBtnClicked(open)}
           >
-            <MenuItem onClick={() => this.handleMenuBtn(false)}>홈</MenuItem>
-            <MenuItem onClick={() => this.handleMenuBtn(false)}>포스트</MenuItem>
+            <MenuItem onClick={() => this.handleSideBtnClicked('/')}>홈</MenuItem>
+            <MenuItem onClick={() => this.handleSideBtnClicked('/about')}>About</MenuItem>
           </Drawer>
           {SplitMe && <SplitMe /> /*유효하면 띄워 줌. */}
           {/* <button onClick={this.showSplitMe}>ClickMe</button> */}
@@ -64,6 +70,7 @@ class App extends React.Component<Props, State> {
           <Route path="/posts/:id" component={Post} />
           <Route path="/test" component={Test} />
         </div>
+        </Router>
       </MuiThemeProvider>
     );
   }
